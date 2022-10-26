d02e4801fdf9c219604787c52d69c20797223383
Zufar Fainan <mzufarfainan@gmail.com>
2022-10-26 10:33:05 +0700

    merge with old code

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var coursesRouter = require('./routes/courses');
+var mediaRouter = require('./routes/media');
+var ordersRouter = require('./routes/orders');
+var paymentsRouter = require('./routes/payments');
 var paijoRouter = require('./routes/paijo');
 
 var app = express();
@@ -20,5 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/paijo', paijoRouter);
+app.use('/courses', coursesRouter);
+app.use('/media', mediaRouter);
+app.use('/orders', ordersRouter);
+app.use('/payments', paymentsRouter);
 
 module.exports = app;
